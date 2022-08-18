1029c242fc17cc4a4506d1177fddf3586789fda4
TrIcKyMaN-VS <vasanthsurya484@gmail.com>
2022-08-19 01:11:36 +0530

    eventinfo pgbknd

--- Backend/routes/eventInfo.js
+++ Backend/routes/eventInfo.js
@@ -6,10 +6,20 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+const auth = require("../middleware/auth");
+const { EventForm } = require('../model/eventModel');
 
-router.get("/", (req,res) => {
+router.get("/",auth, (req,res) => {
     console.log("req rec eventinfo");
-    
+    EventForm.find({userId : req.id}, (err,doc) => {
+        if(err){
+            res.status(400).send(err)
+            console.log(err);
+        }else{
+            console.log(doc[0]);
+            res.send("got it")
+        }
+    })
 })
 
 
