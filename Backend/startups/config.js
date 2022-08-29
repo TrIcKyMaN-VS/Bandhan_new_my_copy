6bd1b20090574d059656ca6386159bcd175fc6e0
VulnExe <jerryjvj33@gmail.com>
2022-08-29 22:33:50 +0530

    dd

--- Backend/startups/config.js
+++
@@ -1,8 +0,0 @@
-const config = require('config');
-
-module.exports = function() {
-    if(!process.env.jwtPrivateKey ){
-        console.error('FATAL ERROR: jwtprivateKey is not defined');
-        process.exit(1)
-    }
-}
